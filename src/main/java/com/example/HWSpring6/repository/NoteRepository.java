57e136a8163724ea2ea9d555dcde75647563a649
Sount <darkzxc@gmail.com>
2024-05-12 15:35:24 +0300

    fix NoteRepository

--- src/main/java/com/example/HWSpring6/repository/NoteRepository.java
+++ src/main/java/com/example/HWSpring6/repository/NoteRepository.java
@@ -1,8 +1,8 @@
 package com.example.HWSpring6.repository;
 
-import com.example.HWSpring6.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.HWSpring6.model.Note;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {}
